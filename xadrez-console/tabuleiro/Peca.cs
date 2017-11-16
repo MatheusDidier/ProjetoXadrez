9e07855ba7247d033f027f810a778f5df0f69753
Didier <matheus.didier@gmail.com>
2017-11-16 11:59:09 -0200

    Adição de algumas classes: tabuleiro, peca e cor

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor{ get; protected set; }
+        public int QuantidadeMovimento { get; protected set; }
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            this.Posicao = posicao;
+            this.Tabuleiro = tabuleiro;
+            this.Cor = cor;
+            this.QuantidadeMovimento = 0;
+        }
+
+         
+
+
+    }
+}
---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xadrez_console.tabuleiro
+{
+    class Tabuleiro
+    {
+        public int Linha{get;set;}
+        public int Coluna{get;set;}
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+            pecas = new Peca[linha, coluna];
+        }
+    }
+}
